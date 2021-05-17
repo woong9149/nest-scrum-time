e715c8553fc2b958dff6fd7659ff0a71e0e2cec6
woong9149 <sewoong0409@naver.com>
2021-05-18 08:38:37 +0900

    210518

--- src/scrum/repository/scrum.repository.ts
+++ src/scrum/repository/scrum.repository.ts
@@ -12,34 +12,9 @@ export class ScrumRepository {
 		private readonly scrumEntityRepository: Repository<ScrumEntity>
 	) {}
 
-	async findAll(request): Promise<Scrum> {
+	async findAll(request): Promise<ScrumEntity[]> {
 		let entities: ScrumEntity[] = await this.scrumEntityRepository.find(request);
-		let len = entities.length;
-		let total = 0;
-		let minimumPoint = 0;
-		let maximumPoint = 0;
-		for(let i = 0; i < len; i++) {
-			total += entities[i].storyPoint;
-
-			minimumPoint = minimumPoint === 0 
-				? entities[i].storyPoint
-				: (minimumPoint > entities[i].storyPoint 
-						? entities[i].storyPoint
-						: minimumPoint);
-
-				maximumPoint = maximumPoint < entities[i].storyPoint 
-			? entities[i].storyPoint
-			: maximumPoint ;
-		}
-		const averagePoint = Math.ceil(total / len);
-	
-		return new Scrum({
-			storySerial: entities[0].storySerial,
-			workerId: entities[0].workerId,
-			averagePoint: averagePoint,
-			minimumPoint: minimumPoint,
-			maximumPoint: maximumPoint,
-		});
+		return entities;
 	}
 
 	async createOne(request):Promise<Scrum> {
--- src/scrum/service/scrum.service.ts
+++ src/scrum/service/scrum.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from "@nestjs/common";
 import { Scrum } from "../domain/scrum";
+import { ScrumEntity } from "../entity/scrum.entity";
 import { ScrumRepository } from "../repository/scrum.repository";
 import { ScrumServiceCreateRequest, ScrumServiceFindAllRequest } from "./dto/scrum.service.dto";
 
@@ -11,12 +12,44 @@ export class ScrumService {
 
 	async findAll(request: ScrumServiceFindAllRequest): Promise<Scrum> {
 		let storyPoint = await this.scrumRepository.findAll(result => result.storySerial === request);
-		return storyPoint;
+		let calculatedStoryPoint = this.getCalculatedStoryPoint(storyPoint);
+		return calculatedStoryPoint;
 	};
 
 	async create(request: ScrumServiceCreateRequest): Promise<Scrum> {
 		let savedPoint = await this.scrumRepository.createOne(request);
 		return;
 	};
+
+	async getCalculatedStoryPoint (request: ScrumEntity[]): Promise<Scrum> {
+		let len = request.length;
+		let total = 0;
+		let minimumPoint = 0;
+		let maximumPoint = 0;
+
+		for(let i = 0; i < len; i++) {
+			total += request[i].storyPoint;
+
+			minimumPoint = minimumPoint === 0 
+				? request[i].storyPoint
+				: (minimumPoint > request[i].storyPoint 
+						? request[i].storyPoint
+						: minimumPoint);
+
+			maximumPoint = maximumPoint < request[i].storyPoint 
+				? request[i].storyPoint
+				: maximumPoint ;
+		}
+
+		const averagePoint = Math.ceil(total / len);
+
+		return new Scrum({
+			storySerial: request[0].storySerial,
+			workerId: request[0].workerId,
+			averagePoint: averagePoint,
+			minimumPoint: minimumPoint,
+			maximumPoint: maximumPoint,
+		});
+	}
 
 }
