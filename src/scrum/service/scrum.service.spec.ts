8c1317ebc31c4c662f3c197aee12535c6b821f2c
woong9149 <sewoong0409@naver.com>
2021-05-24 21:30:39 +0900

    210524

--- src/scrum/service/scrum.service.spec.ts
+++ src/scrum/service/scrum.service.spec.ts
@@ -1,16 +1,37 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ScrumController } from '../controller/scrum.controller';
+import { ScrumEntity } from '../entity/scrum.entity';
 import { ScrumRepository } from '../repository/scrum.repository';
 import { ScrumService } from './scrum.service';
 
 describe('ScrumService', () => {
-  let service: ScrumService;
+  let scrumService: ScrumService;
+  let scrumRepository : ScrumRepository;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [ScrumService, ScrumRepository,],
+      imports: [
+        TypeOrmModule.forRoot({
+          type:'mysql',
+          host: 'localhost',
+          port: 3306,
+          username: 'root',
+          password: '111111',
+          database: 'opentutorials',
+          autoLoadEntities: true,
+          synchronize: true,
+        }),
+        TypeOrmModule.forFeature([
+          ScrumEntity,
+        ])
+      ],
+      controllers: [ScrumController]
     }).compile();
 
-    service = module.get<ScrumService>(ScrumService);
+    scrumService = module.get<ScrumService>(ScrumService);
+    scrumRepository = module.get<ScrumRepository>(ScrumRepository);
   });
 
   // it('should be defined', () => {
