afb5d3dc2b6a48b2cf679fd1bfa5bdf5e7798bec
woong9149 <sewoong0409@naver.com>
2021-06-14 21:43:17 +0900

    210614

--- src/scrum/service/scrum.service.spec.ts
+++ src/scrum/service/scrum.service.spec.ts
@@ -1,37 +1,47 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { getRepositoryToken, TypeOrmModule } from '@nestjs/typeorm';
 import { ScrumController } from '../controller/scrum.controller';
+import { Scrum } from '../domain/scrum';
 import { ScrumEntity } from '../entity/scrum.entity';
 import { ScrumRepository } from '../repository/scrum.repository';
 import { ScrumService } from './scrum.service';
 
+const mockRepository = () => ({
+  findAll: jest.fn(),
+  createOne: jest.fn(),
+});
+
 describe('ScrumService', () => {
   let scrumService: ScrumService;
-  let scrumRepository : ScrumRepository;
+  // let scrumRepository : ScrumRepository;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ScrumService, ScrumRepository,],
-      imports: [
-        TypeOrmModule.forRoot({
-          type:'mysql',
-          host: 'localhost',
-          port: 3306,
-          username: 'root',
-          password: '111111',
-          database: 'opentutorials',
-          autoLoadEntities: true,
-          synchronize: true,
-        }),
-        TypeOrmModule.forFeature([
-          ScrumEntity,
-        ])
+      providers: [
+        ScrumService,
+        { provide: getRepositoryToken(Scrum), useValue: mockRepository()}
       ],
-      controllers: [ScrumController]
+      // providers: [ScrumService, ScrumRepository,],
+      // imports: [
+      //   TypeOrmModule.forRoot({
+      //     type:'mysql',
+      //     host: 'localhost',
+      //     port: 3306,
+      //     username: 'root',
+      //     password: '111111',
+      //     database: 'opentutorials',
+      //     autoLoadEntities: true,
+      //     synchronize: true,
+      //   }),
+      //   TypeOrmModule.forFeature([
+      //     ScrumEntity,
+      //   ])
+      // ],
+      // controllers: [ScrumController]
     }).compile();
 
     scrumService = module.get<ScrumService>(ScrumService);
-    scrumRepository = module.get<ScrumRepository>(ScrumRepository);
+    // scrumRepository = module.get<ScrumRepository>(ScrumRepository);
   });
 
   // it('should be defined', () => {
@@ -40,5 +50,6 @@ describe('ScrumService', () => {
 	
 	it('should be 4', () => {
 		expect(2+2).toEqual(4);
-	})
+  });
+  
 });
