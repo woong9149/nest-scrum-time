b9500bc6180df6c55863dce42649be74d7b98831
woong9149 <sewoong0409@naver.com>
2021-06-21 21:38:40 +0900

    210621

--- src/scrum/service/scrum.service.spec.ts
+++ src/scrum/service/scrum.service.spec.ts
@@ -1,19 +1,23 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { getRepositoryToken, TypeOrmModule } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { ScrumController } from '../controller/scrum.controller';
 import { Scrum } from '../domain/scrum';
 import { ScrumEntity } from '../entity/scrum.entity';
 import { ScrumRepository } from '../repository/scrum.repository';
 import { ScrumService } from './scrum.service';
 
+type MockRepository<T = any> = Partial<Record<keyof Repository<T>, jest.Mock>>;
+
 const mockRepository = () => ({
   findAll: jest.fn(),
   createOne: jest.fn(),
 });
 
+
 describe('ScrumService', () => {
   let scrumService: ScrumService;
-  // let scrumRepository : ScrumRepository;
+  let scrumRepository: MockRepository<Scrum>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -41,7 +45,7 @@ describe('ScrumService', () => {
     }).compile();
 
     scrumService = module.get<ScrumService>(ScrumService);
-    // scrumRepository = module.get<ScrumRepository>(ScrumRepository);
+    scrumRepository = module.get(getRepositoryToken(Scrum));
   });
 
   // it('should be defined', () => {
@@ -51,5 +55,5 @@ describe('ScrumService', () => {
 	it('should be 4', () => {
 		expect(2+2).toEqual(4);
   });
-  
+
 });
